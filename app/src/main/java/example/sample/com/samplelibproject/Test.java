22a19192aac5f1c1a9df78381cb1e3dd8adcdbbc
BoomGitRepo <ravi20101990@gmail.com>
2015-10-13 13:31:27 +0530

    Initial commit

---
+++ app/src/main/java/example/sample/com/samplelibproject/Test.java
@@ -0,0 +1,14 @@
+package example.sample.com.samplelibproject;
+
+import android.util.Log;
+
+/**
+ * Created by ravipa on 10/12/2015.
+ */
+public class Test {
+
+    public static void testLibrary()
+    {
+        Log.d("TAG","libray tested successfully");
+    }
+}
